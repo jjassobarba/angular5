a90db1a6deb7f299adbb6c764bd2162235b23d4e
Angel Jasso <jjassobarba@gmail.com>
2018-03-14 13:57:46 -0600

    Fotos angular firebase
    Subir fotos a firebase con angular y mostrarlas

---
+++ 15. Fotos Firebase/src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { AngularFireStorageModule } from 'angularfire2/storage';
+import { environment } from '../environments/environment';
+
+import { AppComponent } from './app.component';
+import { FotosComponent } from './components/fotos/fotos.component';
+import { CargaComponent } from './components/carga/carga.component';
+import { APP_ROUTES } from './app.routes';
+import { CargaImagenesService } from './services/carga-imagenes.service';
+import { NgDropFilesDirective } from './directives/ng-drop-files.directive';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FotosComponent,
+    CargaComponent,
+    NgDropFilesDirective
+  ],
+  imports: [
+    BrowserModule,
+    APP_ROUTES,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule, // imports firebase/firestore, only needed for database features
+    AngularFireStorageModule // imports firebase/storage only needed for storage features
+  ],
+  providers: [CargaImagenesService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ 15. Fotos Firebase/src/environments/environment.ts
@@ -0,0 +1,16 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyBZ9js6WQo6NU-ffybbrGDA4ed04x8_XtE',
+    authDomain: 'fir-fotos-1d5f8.firebaseapp.com',
+    databaseURL: 'https://fir-fotos-1d5f8.firebaseio.com',
+    projectId: 'fir-fotos-1d5f8',
+    storageBucket: 'fir-fotos-1d5f8.appspot.com',
+    messagingSenderId: '14605751926'
+  }
+};
